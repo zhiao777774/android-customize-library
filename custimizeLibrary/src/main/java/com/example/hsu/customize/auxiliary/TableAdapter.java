c9be52f23fd655e41d1a25011b05623af878e45f
zhiao777774 <47570633+zhiao777774@users.noreply.github.com>
2019-02-13 01:45:17 +0800

    commit

--- custimizeLibrary/src/main/java/com/example/hsu/customize/auxiliary/TableAdapter.java
+++ custimizeLibrary/src/main/java/com/example/hsu/customize/auxiliary/TableAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -58,7 +59,7 @@ public class TableAdapter<T> extends BaseAdapter {
         return convertView;
     }
 
-    public void setViewHolder(ViewHolder<T> h){
+    public void setViewHolder(@NotNull ViewHolder<T> h){
         this.mViewHolder = h;
     }
 

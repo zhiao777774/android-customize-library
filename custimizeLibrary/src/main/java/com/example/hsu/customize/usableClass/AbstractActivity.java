cccf030e0965e6967b36c05e3ced6353512d74a6
zhiao777774 <47570633+zhiao777774@users.noreply.github.com>
2019-04-11 22:59:52 +0800

    optimize AbstractActivity: change create method have one parameter "saveInstanceState" and insert property savedInstanceState can be use to record current state

--- custimizeLibrary/src/main/java/com/example/hsu/customize/usableClass/AbstractActivity.java
+++ custimizeLibrary/src/main/java/com/example/hsu/customize/usableClass/AbstractActivity.java
@@ -1,10 +1,12 @@
 package com.example.hsu.customize.usableClass;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class AbstractActivity extends AppCompatActivity {
+    public Bundle savedInstanceState;
 
-    public abstract void create();
+    public abstract void create(Bundle savedInstanceState);
 
     public void restart() { }
 
